9c0ce1b694fea2d956313edba3cc0bc2d6af0664
Unknown <kevin.moore.138@gmail.com>
2017-03-14 22:06:25 -0700

    Get All user info by ID
    Check if user exists by id

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -29,5 +29,38 @@
       });
   }
 
+  UserController.checkIfUserExists = (req, res) => {
+    const reqFields = _.values(_.pick(
+      req.body,
+      'user_id'
+    ));
+
+    const queryStr = 'SELECT CASE WHEN EXISTS (SELECT * from person WHERE user_id = $1) THEN CAST(1 AS BIT) ELSE CAST (0 AS BIT) END'
+    pool.query(queryStr, reqFields)
+    .then((result) => {
+      if (result.rows[0].case === 1) res.json({ exists: true });
+      else res.json({ exists: false });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+  }
+
+  UserController.getUserById = (req, res) => {
+    const reqFields = _.values(_.pick(
+      req.body,
+      'user_id'
+    ));
+
+    const queryStr = 'SELECT * from person where user_id = $1'
+    pool.query(queryStr, reqFields)
+    .then((result) => {
+      res.json(result.rows);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+  }
+
   module.exports = UserController;
 })();
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.get('/', controller.allUsers);
 router.post('/', controller.addUser);
+router.post('/userById', controller.getUserById);
+router.post('/checkIfExistsById', controller.checkIfUserExists);
 
 module.exports = router;
