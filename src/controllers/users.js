5296875731fee239621d56c859ad5219acd0493e
Kevin Moore <kevin.moore.138@gmail.com>
2017-03-15 21:28:27 -0700

    Convert to knex queries
    To write easier and cleaner queries we are using Knex and foregoing an ORM.

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,32 +1,35 @@
 (() => {
   const _ = require('underscore');
-  const pool = require('../../server');
+  const app = require('../../server');
   const UserController = {};
 
   UserController.allUsers = (req, res) => {
-    pool.query('SELECT * from person', (err, result) => {
-      if (err) res.status(500).json({ error: err.message });
-      res.json(result.rows);
+    app.knex.select('*').from('person')
+    .then((rows) => {
+       res.json(rows);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
     });
   }
 
   UserController.addUser = (req, res) => {
-    const reqFields = _.values(_.pick(
+    const reqFields = _.pick(
       req.body,
       'user_id',
       'user_name',
       'first_name',
       'last_name',
       'email'
-    ));
-    const queryStr = 'INSERT INTO PERSON (user_id, user_name, first_name, last_name, email) VALUES ($1, $2, $3, $4, $5)';
-    pool.query(queryStr, reqFields)
-      .then(() => {
-        res.json({status: 'success'});
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
+    );
+    
+    app.knex.insert(reqFields).into('person')
+    .then(() => {
+      res.json({status: 'success'});
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
   }
 
   module.exports = UserController;
